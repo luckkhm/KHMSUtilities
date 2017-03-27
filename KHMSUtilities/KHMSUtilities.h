436c6bd5a14eec80d08428de72a6e56062710552
AhmedKhemiri <ahmedkhemirigl@gmail.com>
2017-03-27 12:08:22 +0100

    Initial Commit

---
+++ KHMSUtilities/KHMSUtilities.h
@@ -0,0 +1,19 @@
+//
+//  KHMSUtilities.h
+//  KHMSUtilities
+//
+//  Created by Ahmed Khemiri on 3/27/17.
+//  Copyright © 2017 Ahmed Khemiri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KHMSUtilities.
+FOUNDATION_EXPORT double KHMSUtilitiesVersionNumber;
+
+//! Project version string for KHMSUtilities.
+FOUNDATION_EXPORT const unsigned char KHMSUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KHMSUtilities/PublicHeader.h>
+
+
